8d2219b2276c45f6f7eb786ffe12f36ffeb8aaec
vadthyavath <jumani.pro.geek@gmail.com>
2014-07-28 17:15:50 +0530

    method to Generate Binary Numbers from 1 to n with running time O(nlogn)

--- generatebinarynums.cpp
+++ generatebinarynums.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+//http_proxy=http://your.proxy.server:proxy_port
+//git config --global http.proxy $http_proxy
+//git clone http://git.gnome.org/browse/tracker
+
+
 //program to Generate Binary Numbers from 1 to n
 //g++ 4.8.1 doesn't support to_string method so we use old version of g++ in the following way
 //g++ -std=c++11 filename.cpp -o outputfilename
@@ -13,16 +18,15 @@ int main()
 		for (int i=2;i<=a;i++)
 				{	int m=i;
 					string str1="";
-								while(true)
-										{ 
-											str1=to_string(m%2)+str1;
-											m=m/2;
-		
-											if(m==1)
-												{str1=to_string(1)+str1;
-													break;
-												}
-										}
+					while(true)
+						{ 
+							str1=to_string(m%2)+str1;
+							m=m/2;
+							if(m==1)
+							 {str1=to_string(1)+str1;
+								break;
+							 }
+						}
 					cout<<str1<<"\n";
 				}
 
